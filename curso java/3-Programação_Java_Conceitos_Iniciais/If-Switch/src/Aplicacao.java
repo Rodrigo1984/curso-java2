159eeec55c7c3bda804f2dbe4d67e1b27155c320
Rodrigo1984 <32502474+Rodrigo1984@users.noreply.github.com>
2021-01-01 18:16:51 -0300

    subi o curso

--- curso java/3-Programação_Java_Conceitos_Iniciais/If-Switch/src/Aplicacao.java
+++ curso java/3-Programação_Java_Conceitos_Iniciais/If-Switch/src/Aplicacao.java
@@ -3,18 +3,18 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-//		int num = 15;
-//		
-//		if (num > 10) {
-//			System.out.println("O número é maior que 10");
-//		
-//		} else if (num < 10) {
-//			System.out.println("O número é menor que 10");
-//		
-//		} else {
-//			System.out.println("O número é 10");
-//			System.out.println("O número é 10 mesmo!");
-//		}
+		int num = 15;
+		
+		if (num > 10) {
+			System.out.println("O nï¿½mero ï¿½ maior que 10");
+		
+		} else if (num < 10) {
+			System.out.println("O nï¿½mero ï¿½ menor que 10");
+		
+		} else {
+			System.out.println("O nï¿½mero ï¿½ 10");
+			System.out.println("O nï¿½mero ï¿½ 10 mesmo!");
+		}
 		
 		int num = 10;
 		
@@ -24,16 +24,16 @@ public class Aplicacao {
 		case 3:
 		case 4:
 		case 5:
-			System.out.println("O número é 1, 2, 3 4 ou 5");
+			System.out.println("O nï¿½mero ï¿½ 1, 2, 3 4 ou 5");
 			break;
 		case 10:
-			System.out.println("O número é 10");
+			System.out.println("O nï¿½mero ï¿½ 10");
 			break;
 		case 15:
-			System.out.println("O número é 15");
+			System.out.println("O nï¿½mero ï¿½ 15");
 			break;
 		default:
-			System.out.println("O número é outro");
+			System.out.println("O nï¿½mero ï¿½ outro");
 			break;
 		}
 	}
